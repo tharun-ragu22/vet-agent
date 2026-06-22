0e212cf5fec137d88fd8b561bd3c108e053c574e
tharun-ragu22 <tharunragulan@gmail.com>
2026-06-21 22:49:20 -0400

    added global models module

---
+++ agent/models.py
@@ -0,0 +1,24 @@
+from pydantic_ai.models.ollama import OllamaModel
+from pydantic_ai.providers.ollama import OllamaProvider
+from pydantic_ai.models.openrouter import OpenRouterModel
+from pydantic_ai.models.google import GoogleModel
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+
+OPENROUTER_API_KEY = os.getenv("OPENROUTER_API_KEY")
+
+gemma_model = OllamaModel(
+    model_name="gemma4:latest",
+    provider=OllamaProvider(base_url="http://localhost:11434/v1"),
+)
+
+openrouter_model = OpenRouterModel(
+    "google/gemma-4-26b-a4b-it:free",
+)
+
+google_model = GoogleModel(
+    "gemini-3.5-flash"
+)
