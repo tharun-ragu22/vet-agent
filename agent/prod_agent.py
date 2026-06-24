5c2e00b2ae0fd4133a3df233ba7297504ddd2da6
tharun-ragu22 <tharunragulan@gmail.com>
2026-06-24 18:22:35 -0400

    prod get db connection

--- agent/prod_agent.py
+++ agent/prod_agent.py
@@ -1,10 +1,26 @@
 from agent.agent_interface import IAgent, AGENT_SYSTEM_PROMPT
-from pydantic_ai import Agent, AgentRunResult
+from pydantic_ai import Agent, AgentRunResult, RunContext
 from .models import google_model
 import sqlite3
+from dataclasses import dataclass
+
+@dataclass
+class ProdAgentDeps:
+    db_conn : sqlite3.Connection
 
 class ProdAgent(IAgent):
+
+    def __init__(self, db_connection: sqlite3.Connection = None):
+        if db_connection is None:
+            db_connection = sqlite3.connect(":memory:")
+        self.db_conn = db_connection
+        self.deps = ProdAgentDeps(db_connection)
+        self._init_db()
     
+    def _init_db(self):
+        cursor = self.deps.db_conn.cursor()
+        cursor.execute("CREATE TABLE IF NOT EXISTS appointments (patient_name TEXT PRIMARY KEY, day TEXT, time TEXT)")
+        self.deps.db_conn.commit()
 
     _agent = Agent(
         model=google_model,
@@ -12,7 +28,7 @@ class ProdAgent(IAgent):
     )
 
     @staticmethod
-    def make_appointment_impl(patient_name: str, day: str, time: str, db_connection: sqlite3.Connection = None):
+    def make_appointment_impl(patient_name: str, day: str, time: str, db_connection: sqlite3.Connection):
         cursor = db_connection.cursor()
         cursor.execute(
             "INSERT INTO appointments (patient_name, day, time) VALUES (?, ?, ?)",
@@ -20,11 +36,11 @@ class ProdAgent(IAgent):
         )
         db_connection.commit()
     
-    @_agent.tool_plain
-    def make_appointment(patient_name: str, day: str, time: str) -> str:
+    @_agent.tool
+    def make_appointment(ctx: RunContext[ProdAgentDeps], patient_name: str, day: str, time: str) -> str:
         """Makes the appointment in the system"""
         print(f'make_appointment: making appointment for {patient_name} at {day} {time}')
-        ProdAgent.make_appointment_impl(patient_name, day, time)
+        ProdAgent.make_appointment_impl(patient_name, day, time, ctx.deps.db_conn)
 
 
     @_agent.tool_plain
@@ -35,7 +51,7 @@ class ProdAgent(IAgent):
 
     # 4. The run_agent execution wrapper
     async def run_agent(self, prompt: str) -> AgentRunResult[str]:
-        res = await self._agent.run(prompt)
+        res = await self._agent.run(prompt, deps = self.deps)
         return res
 
 
