abdc24216c6f1b88e4b771eb8008e072c8440b7c
tharun-ragu22 <tharunragulan@gmail.com>
2026-07-20 14:01:48 -0400

    can add and see a message

--- context_store/context_store.py
+++ context_store/context_store.py
@@ -1,4 +1,22 @@
+from dataclasses import dataclass
+
+@dataclass
+class ConversationTurn:
+    role: str
+    content: str
+
 class ContextStore:
-    def get_context(self, call_sid : str) -> list[dict[str, str]]:
+    _store : dict[str, list[ConversationTurn]]= {}
+    def get_context(self, call_sid : str) -> list[ConversationTurn]:
         """Gets the context of a conversation"""
-        return []
+        return self._store.get(call_sid, [])
+    def add_message(self, call_sid: str, role: str, content: str) -> None:
+        if call_sid not in self._store:
+            self._store[call_sid] = []
+        
+        self._store[call_sid].append(
+            ConversationTurn(
+                role=role,
+                content=content
+            )
+        )
--- context_store/test_context_store.py
+++ context_store/test_context_store.py
@@ -1,5 +1,16 @@
-from context_store import ContextStore
+from context_store import ContextStore, ConversationTurn
 def test_context_store_nonexistant_id_gets_nothing():
     store = ContextStore()
     context = store.get_context(call_sid='1234')
     assert len(context) == 0
+
+def test_context_store_existing_id_should_get_result():
+    store = ContextStore()
+    store.add_message(call_sid = '1234', role = 'user', content = 'hi there')
+    
+    context = store.get_context(call_sid='1234')
+    assert len(context) == 1
+
+    msg = context[0]
+    assert msg.role == 'user'
+    assert msg.content == 'hi there'
