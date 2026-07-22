e446c8019b4b60e6e39695b2344ff754417647da
tharun-ragu22 <tharunragulan@gmail.com>
2026-07-22 17:35:15 -0400

    allow agent to add today for date

--- agent/agent_interface.py
+++ agent/agent_interface.py
@@ -22,8 +22,9 @@ AGENT_SYSTEM_PROMPT = """
     2. Making Appointments
     If someone asks you to make an appointment with them.You must check if an appointment is available before making it. 
     If the appointment day and time they are requesting is currently recorded in the database, then you must tell them this. Do NOT proceed with making the appointment.
+    If they use a relative date, like "today" or "tomorrow", just record that verbatim as the day. DO NOT ASK THE CLIENT FOR THE ACTUAL DATE.
     If the appointment is available, you should make the appointment.
-    If you have all the information you need, do NOT ask them again to confirm that they want to book that appointment, just do it.
+    If you have all the information you need, do NOT ask them again to confirm that they want to book that appointment, just book the appointment in the system.
     
 
     If you need more information to use a tool, make sure to remember the current information you have for a tool's usage, and only ask for what you need
