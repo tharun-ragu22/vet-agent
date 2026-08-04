d88ceba3e079b855b4e9dd0f1d13161335eb398f
tharun-ragu22 <tharunragulan@gmail.com>
2026-08-04 18:26:06 -0400

    works when today is after given weekday in week

--- agent/test_agent_base.py
+++ agent/test_agent_base.py
@@ -172,10 +172,22 @@ def test_get_datetime_from_phrase_today_at_five_pm():
 
 @freeze_time("2026-08-03 12:00:00")
 def test_get_datetime_from_phrase_today_is_monday_get_thursday_at_11_am():
-    # Given the agent has parsed out 'today at 5 P.M.' as the day to make the appointment
+    # Given today is Monday
+    # And the agent has parsed out 'thursday at 11 A.M.' as the day to make the appointment
     DATE_PHRASE = 'thursday at 11 A.M.'
     # When the agent uses the tool on this string
     acc_datetime = AgentBaseClass.get_datetime_from_phrase_impl(DATE_PHRASE)
     # Then the tool returns the correct datetime
     expected_date = (datetime.now()+timedelta(days=3)).replace(hour=11, minute=0, second = 0, microsecond=0)
+    assert acc_datetime == expected_date
+
+@freeze_time("2026-08-07 12:00:00")
+def test_get_datetime_from_phrase_today_is_monday_get_thursday_at_11_am():
+    # Given today is Friday
+    # Given the agent has parsed out 'thursday at 11 A.M.' as the day to make the appointment
+    DATE_PHRASE = 'thursday at 11 A.M.'
+    # When the agent uses the tool on this string
+    acc_datetime = AgentBaseClass.get_datetime_from_phrase_impl(DATE_PHRASE)
+    # Then the tool returns the correct datetime
+    expected_date = (datetime.now()+timedelta(days=6)).replace(hour=11, minute=0, second = 0, microsecond=0)
     assert acc_datetime == expected_date
